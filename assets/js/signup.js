7622dc69343ba7ea573afb9bb1c32b74c0735352
Avinav.Nakarmi <avinav890@gmail.com>
2023-09-12 14:13:03 +0545

    final responsive changed

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,4 +1,5 @@
 import {user_detail} from "../js/signin.js";
+const user_detail = require("../js/signin.js")
 user_detail.forEach(user => {
 
     console.log(`email ${user.email}, password${user.password}`);
